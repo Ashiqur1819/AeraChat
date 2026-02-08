ec7f7422199fa5af81d82353da982fd720c4368d
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-02-08 08:38:29 +0600

    Connect socket function

--- client/context/authContext.jsx
+++ client/context/authContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios"
 import toast from "react-hot-toast";
+import {io} from "socket.io-client"
 
 const backendUrl = import.meta.env.BACKEND_URL
 axios.defaults.baseURL = backendUrl
@@ -21,6 +22,7 @@ export const AuthProvider = ({children}) => {
 
            if(data.success){
             setAuthUser(data.user)
+            connectSocket(data.user)
            }
         } catch (error) {
             toast.error(error.message)
@@ -28,7 +30,21 @@ export const AuthProvider = ({children}) => {
     }
 
     // Connect socket function
-    
+    const connectSocket = (userData) => {
+        if(!userData || !socket?.connected) return
+
+        const newSocket = io(backendUrl, {
+            query: {
+                userId: userData._id
+            }
+        })
+        newSocket.connect()
+        setSocket(newSocket)
+
+        newSocket.on("getOnlineUsers", (userIds) => {
+            setOnlineUsers(userIds)
+        })
+    }
 
     useEffect(() => {
         if(token){
