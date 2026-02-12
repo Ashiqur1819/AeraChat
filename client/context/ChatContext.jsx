8650dd9828d254cf2526071a03a627b5c58c9f1b
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-02-12 12:09:36 +0600

    Create a function for subscribe to messages for selected user

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -54,6 +54,24 @@ export const ChatProvider = ({ children }) => {
     }
   };
 
+
+  // Subscribe to messages for selected user
+  const subscribeToMessages = async () => {
+    if(!socket) return
+
+    socket.on("newMessage", (newMessage) => {
+      if(selectedUser && newMessage.senderId === selectedUser._id){
+        newMessage.seen = true
+        setMessages((prevMessages) => [...prevMessages, newMessage])
+        axios.put(`/api/messages/mark/${newMessage._id}`)
+      }else{
+        setUnseenMessages((prevUnseenMessages) => ({
+          ...prevUnseenMessages, [newMessage.senderId] : prevUnseenMessages[newMessage.senderId] ? prevUnseenMessages[newMessage.senderId] + 1 : 1
+        }))
+      }
+    })
+  }
+
   const value = {};
 
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
